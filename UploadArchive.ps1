55f18fcef5ff768d81f432aa747b8edcf1aaa304
ChrisVickersKyndryl <100736659+ChrisVickersKyndryl@users.noreply.github.com>
2023-07-06 22:05:01 +1000

    Update UploadArchive.ps1

--- UploadArchive.ps1
+++ UploadArchive.ps1
@@ -5,21 +5,21 @@ param(
   $StorageUser
 )
 
-# Variables
-$destinationPath = "$Destination\$($env:computername)\"
-          
 # Read the encrypted password from the text file
 $securePassword = Get-Content -Path $PasswordFile | ConvertTo-SecureString -AsPlainText -Force
           
 # Create a PSCredential object with the network credentials
 $credential = New-Object System.Management.Automation.PSCredential($StorageUser, $securePassword)
-         
+
+# Temporarily map drive
+New-PSDrive -Name "A" -PSProvider FileSystem -Root $Destination -Credential $credential
+
 # Get all files in folder than end with .evtv
 Get-ChildItem -Path $SourcePath -Filter "*.evtx" | ForEach-Object {
   # Create folder of server name, if it doesn't exist.
-  If(!(test-path -PathType container $destinationPath)) {
+  If(!(test-path -PathType container "A:\$($env:computername)\")) {
     New-Item -ItemType Directory -Path $destinationPath
   }
   # Copy file to remote folder as user
-  Move-Item -Path $_.FullName -Destination "$destinationPath$($_.Name)" -Credential $credential
+  Move-Item -Path $_.FullName -Destination "A:\$($env:computername)\$($_.Name)"
 }
