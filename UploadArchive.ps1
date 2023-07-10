8a394d7568a86fbc263a8af922b5a9d19d65b6a5
ChrisVickersKyndryl <100736659+ChrisVickersKyndryl@users.noreply.github.com>
2023-07-11 09:39:50 +1000

    Update UploadArchive.ps1

--- UploadArchive.ps1
+++ UploadArchive.ps1
@@ -11,6 +11,7 @@ $securePassword = $(Get-Content -Path $PasswordFile | ConvertTo-SecureString)
 # Create a PSCredential object with the network credentials
 $credential = New-Object System.Management.Automation.PSCredential($StorageUser, $securePassword)
 
+# Go through drives and check if it is being used
 Foreach ($drvletter in "ABDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray()) {
   # Check if the drive is being used. If yes go to next letter
   if(Get-Volume -FilePath "$drvletter:\"){ continue }
@@ -24,14 +25,17 @@ Foreach ($drvletter in "ABDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray()) {
     If(!(test-path -PathType container "A:\$($env:computername)\")) {
       New-Item -ItemType Directory -Path "A:\$($env:computername)\"
     }
-    # Copy file to remote folder as user
+    
+    # Copy file to remote folder
     Move-Item -Path $_.FullName -Destination "A:\$($env:computername)\$($_.Name)"
   }
-  
+
+  # Remove drive
   Get-PSDrive $drvletter | Remove-PSDrive -Force -Verbose
 
   # Exit with success return code
   Exit 0
 }
 
-Exit 1
+# No available drives
+Exit 10
