1da6edff643d0984481981feeeb4f1e022990f79
ChrisVickersKyndryl <100736659+ChrisVickersKyndryl@users.noreply.github.com>
2023-07-11 09:37:10 +1000

    Update UploadArchive.ps1

--- UploadArchive.ps1
+++ UploadArchive.ps1
@@ -11,19 +11,27 @@ $securePassword = $(Get-Content -Path $PasswordFile | ConvertTo-SecureString)
 # Create a PSCredential object with the network credentials
 $credential = New-Object System.Management.Automation.PSCredential($StorageUser, $securePassword)
 
-# Temporarily map drive
-New-PSDrive -Name "A" -PSProvider FileSystem -Root $Destination -Credential $credential
-
-# Get all files in folder than end with .evtv
-Get-ChildItem -Path $SourcePath -Filter "*.evtx" | ForEach-Object {
-  # Create folder of server name, if it doesn't exist.
-  If(!(test-path -PathType container "A:\$($env:computername)\")) {
-    New-Item -ItemType Directory -Path "A:\$($env:computername)\"
+Foreach ($drvletter in "ABDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray()) {
+  # Check if the drive is being used. If yes go to next letter
+  if(Get-Volume -FilePath "$drvletter:\"){ continue }
+  
+  # Temporarily map drive
+  New-PSDrive -Name $drvletter -PSProvider FileSystem -Root $Destination -Credential $credential
+  
+  # Get all files in folder than end with .evtv
+  Get-ChildItem -Path $SourcePath -Filter "*.evtx" | ForEach-Object {
+    # Create folder of server name, if it doesn't exist.
+    If(!(test-path -PathType container "A:\$($env:computername)\")) {
+      New-Item -ItemType Directory -Path "A:\$($env:computername)\"
+    }
+    # Copy file to remote folder as user
+    Move-Item -Path $_.FullName -Destination "A:\$($env:computername)\$($_.Name)"
   }
-  # Copy file to remote folder as user
-  Move-Item -Path $_.FullName -Destination "A:\$($env:computername)\$($_.Name)"
+  
+  Get-PSDrive $drvletter | Remove-PSDrive -Force -Verbose
+
+  # Exit with success return code
+  Exit 0
 }
 
-Get-PSDrive "A" | Remove-PSDrive -Force -Verbose
-
-Exit 0
+Exit 1
